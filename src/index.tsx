30d4be12834a7222e2738fe6491a124f761b3abd
truth <truth@mba.local>
2021-05-27 18:23:39 +0900

    options、screenOptionsで受け取れる引数

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack'
+import {CommonActions} from '@react-navigation/routers';
+import {createStackNavigator, HeaderTitle} from '@react-navigation/stack'
 import {useNavigation, NavigationContainer, NavigationHelpersContext, DrawerActions, TabRouter,} from '@react-navigation/native'
 import {createDrawerNavigator} from '@react-navigation/drawer'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { NativeViewGestureHandler } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,15 +19,14 @@ function Main() {
   const {navigate} = useNavigation();
   return (
     <View style={styles.container}>
-      <Text>Main</Text>
-      <TouchableOpacity
-        onPress={() => {
+       <TouchableOpacity
+        onPress={() =>
           navigate('Sub', {
-            title: 'from Main',
-          });
-        }}>
-        <Text>go to sub</Text>
-      </TouchableOpacity>
+            title: `${Date.now()}`,
+          })
+        }>
+        <Text>go to Sub</Text>
+       </TouchableOpacity>
     </View>
   );
 }
@@ -62,13 +63,41 @@ function Sub() {
 const Stack = createStackNavigator();
 
 function StackNavigator() {
-  return(
-    <Stack.Navigator
-      screenOptions={({route}) => ({
-        title: route.params && route.params.title,
-    })}>
+  return (
+    <Stack.Navigator>
       <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="Sub" component={Sub} />
+      <Stack.Screen
+        name="Sub"
+        component={Sub}
+        options={({navigation, route}) => {
+          const title =
+            route.params && route.params.title ? route.params.title : '0';
+          const seed = parseInt(title, 10) % 3;
+          switch (seed) {
+            case 0:
+              console.log('goback');
+              navigation.goBack();
+              break;
+            case 1:
+              console.log('replace');
+              navigation.replace('Main');
+              break;
+            default:
+              console.log('reset');
+              const action = CommonActions.reset({
+                index: 0,
+                routes: [
+                  {
+                    name: 'Main',
+                  },
+                ],
+              });
+              navigation.dispatch(action);
+              break;
+          }
+          return {title};
+        }}
+      />
     </Stack.Navigator>
    );
  }
